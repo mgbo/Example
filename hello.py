23d878f73f18236b5a205c4d3bf04ef8c04a71fc
mgbo <mgbo433@gmail.com>
2019-11-10 15:50:02 +0300

    add all modified file

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # I'm adding new features and changes to your repo.....
 print ('hello, github')
 print ('Здравствуйте!!!')
-print ('နေကောင်းလား')
+# print ('နေကောင်းလား')
 print ('hello, all my students')
